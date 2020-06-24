e384191522d6e12d0f3a443b4af58fd8d9b02b0b
Guillaume Clochard <guillaume.clochard@iadvize.com>
2020-06-24 16:52:07 +0200

    feat: add or guard helper

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+-   rename `combineGuards` as `and`
+
+### Added 
+
+-   Add `or` guard helpers
+
 ## [1.0.5]
 
 ### Changed
--- src/guard.ts
+++ src/guard.ts
@@ -1,5 +1,39 @@
-import { Guard, NotGuard } from './types';
+import { Guard, NotGuard, UnionToIntersection, GuardedType } from './types';
 
-export const not = <A, B extends A>(guard: Guard<A, B>): NotGuard<A, B> => (
-  value: A,
-): value is Exclude<A, B> => !guard(value);
+export function not<A, B extends A>(guard: Guard<A, B>): NotGuard<A, B> {
+  return (value: A): value is Exclude<A, B> => !guard(value);
+}
+
+export function and<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Guards extends Guard<any, any>[]
+>(...guards: Guards) {
+  type InputTypes = Parameters<Guards[number]>[0];
+  type IntersectionOfInputTypes = UnionToIntersection<InputTypes>;
+
+  type OuputTypes = GuardedType<Guards[number]>;
+  type IntersectionOfOutputTypes = UnionToIntersection<OuputTypes>;
+
+  return (t: IntersectionOfInputTypes): t is IntersectionOfOutputTypes => {
+    return guards.reduce(
+      (acc: boolean, guard: Guard<unknown, unknown>) => acc && guard(t),
+      true,
+    );
+  };
+}
+
+export function or<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Guards extends Guard<any, any>[]
+>(...guards: Guards) {
+  type InputTypes = Parameters<Guards[number]>[0];
+  type IntersectionOfInputTypes = UnionToIntersection<InputTypes>;
+
+  type OuputTypes = GuardedType<Guards[number]>;
+
+  return (
+    t: IntersectionOfInputTypes,
+  ): t is IntersectionOfInputTypes & OuputTypes => {
+    return guards.some((guard: Guard<unknown, unknown>) => guard(t));
+  };
+}
